dc009eeea3bbc2efb0ba14d346b82c8ffc495d93
KwekuDar <kwekudar@outlook.com>
2022-01-12 00:22:26 -0500

    fixed icons and images not showing with ejs

--- routes/users.js
+++ routes/users.js
@@ -21,5 +21,33 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+  router.get("/", (req, res) => {
+    res.render("index")
+  });
+
+  router.get("/login/:id", (req, res) => {
+    const userId = req.params.id;
+    req.session.userId = userId;
+        
+    db.query(`SELECT * FROM users WHERE id = $1;`,[userId])
+      .then(data => {
+        const users = data.rows;
+        req.session.user = users.name;
+        res.json({ users });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+
+    res.redirect("/");
+  });
+  router.post("/logout", (req, res) => {
+    req.session.userId = null;
+    req.session.user = null;
+    res.redirect("/");
+  });
   return router;
 };
